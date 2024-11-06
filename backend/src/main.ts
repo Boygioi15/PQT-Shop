83f58bdadfdc0b176bcaa46a923912f5065a3eec
HoangGia Phong <144602252+Winder510@users.noreply.github.com>
2024-11-06 11:42:29 +0700

    enable cors config cookie parser

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+  //cors config
+  app.enableCors({
+    credentials: true,
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
+
+  // config cookied parser
+  app.use(cookieParser());
 
   await app.listen(configService.get<string>('PORT'));
 }
