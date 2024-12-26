1361dde265f9a7cdfb54aed23ce34de6d44bae1b
HoangGia Phong <144602252+Winder510@users.noreply.github.com>
2024-12-26 23:57:24 +0700

    fix cart

--- backend/backend-nodejs/src/services/cart.service.js
+++ backend/backend-nodejs/src/services/cart.service.js
@@ -167,6 +167,118 @@ export class CartService {
         return deleteCart;
     }
 
+    static async showCarts({
+        userId
+    }) {
+        if (!userId) {
+            throw new BadRequestError("You have signed out")
+        }
+        const cart = await cartModel
+            .findOne({
+                cart_userId: userId,
+            })
+            .lean();
+
+        if (!cart || !cart.cart_products || cart.cart_products.length === 0) {
+            return {
+                message: 'Giỏ hàng trống',
+                cartItems: [],
+            };
+        }
+
+        const items = await Promise.all(cart.cart_products.map(async (item) => {
+            const {
+                skuId,
+                quantity
+            } = item
+
+            let data = await getProductInforForCart(skuId)
+
+            return {
+                ...data,
+                quantity,
+            }
+        }));
+
+        return items
+    }
+
+    static async replaceItemInCarts({
+        userId,
+        oldSkuId,
+        newSkuId,
+    }) {
+        const userCart = await cartModel.findOne({
+            cart_userId: userId
+        }).lean();
+        if (!userCart) {
+            throw new NotFoundError("Giỏ hàng không tồn tại");
+        }
+
+        let newProduct = await findSkuById(
+            newSkuId
+        );
+
+        const itemIndex = userCart.cart_products.findIndex(item => item.skuId?.toString() === oldSkuId);
+        if (itemIndex === -1) {
+            throw new NotFoundError("Sản phẩm cũ không tồn tại trong giỏ hàng");
+        }
+
+        const currentQuantity = userCart.cart_products[itemIndex].quantity;
+
+        if (currentQuantity > newProduct.product_quantity) throw new BadRequestError("Số lượng sản phầm không đủ")
+
+        return await replaceItemsInCart({
+            userId,
+            oldSkuId,
+            newSkuId,
+        })
+
+    }
+
+    static async clearCarts({
+        userId
+    }) {
+        const cart = await cartModel.find({
+            cart_userId: userId
+        })
+        cart.cart_products = []
+        cart.save()
+        return {
+            success: true
+        }
+    }
+
+    static async getCartByUserIsd({
+        userId
+    }) {
+        return cartModel.findOne({
+            cart_userId: new Types.ObjectId(userId)
+        })
+    }
+
+    static getProductInforForLocals = async ({
+        carts
+    }) => {
+        const items = await Promise.all(carts.map(async (item) => {
+            const {
+                skuId,
+                quantity
+            } = item
+
+            let data = await getProductInforForCart(skuId)
+
+            return {
+                ...data,
+                quantity,
+            }
+        }));
+
+        return items
+    }
+
+
+
     static async showCart({
         userId
     }) {
@@ -276,6 +388,5 @@ export class CartService {
 
         return items
     }
-
 
 }
